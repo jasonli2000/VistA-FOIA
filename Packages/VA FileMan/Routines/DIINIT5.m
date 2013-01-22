ac9987ef8e5a3daed7b262f54f96d0c4238a3754
Jason Li <jason.li@kitware.com>
2013-01-22 12:25:47 -0500

    Fileman 22p2 changes
    Ignore the changes that are not caused by Fileman 22.2.
    
    Change-Id: Iaf4bb9052d3f1aef3c30ca517c782f2e82ba9caa

---
+++ Packages/VA FileMan/Routines/DIINIT5.m
@@ -0,0 +1,6 @@
+DIINIT5	; ; 13-DEC-2012
+	;;22.2T1;VA FILEMAN;;Dec 14, 2012
+	;Per VHA Directive 2004-038, this routine should not be modified.
+	K ^UTILITY("DIF",$J) S DIFRDIFI=1 F I=1:1:0 S ^UTILITY("DIF",$J,DIFRDIFI)=$T(IXF+I),DIFRDIFI=DIFRDIFI+1
+	Q
+IXF	;;VA FILEMAN^DI
---
+++ Packages/VA FileMan/Routines/DIISC.m
@@ -0,0 +1,7 @@
+DIISC	;GFT/GFT - SAVE AS '%ZISC' IF YOU DON'T HAVE A '%ZISC' ROUTINE;27OCT2011
+	;;22.2T1;VA FILEMAN;;Dec 14, 2012
+	;Per VHA Directive 2004-038, this routine should not be modified.
+	;;
+%ZISC	;
+	I IO'=$P D
+	.C IO
